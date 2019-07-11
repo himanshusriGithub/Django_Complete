34eaaa2ade36b7d3d60d4a5e7cbb308bab4e09e4
Webber <51797309+himanshusriGithub@users.noreply.github.com>
2019-07-11 15:32:55 +0530

    Database creation

--- start_Pro/blog/views.py
+++ start_Pro/blog/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
+from.models import Post
 
-posts=[
-    {
-        'author':'coreyMS',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'August 27,2019'
-    },
-    {
-        'author':'kaleyeyMS',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'August 28,2019'
-    }
-]
+# posts=[
+#     {
+#         'author':'coreyMS',
+#         'title':'Blog Post 1',
+#         'content':'First post content',
+#         'date_posted':'August 27,2019'
+#     },
+#     {
+#         'author':'kaleyeyMS',
+#         'title':'Blog Post 2',
+#         'content':'Second post content',
+#         'date_posted':'August 28,2019'
+#     }
+# ]
 
 def home(request):
     context={
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request,'blog/home.html',context)
