67186748f281d0915b422b0278e12bfdd4faf0c5
Webber <51797309+himanshusriGithub@users.noreply.github.com>
2019-07-16 22:54:08 +0530

    version 1.8

--- start_Pro/users/forms.py
+++ start_Pro/users/forms.py
@@ -18,7 +18,6 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
-
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
